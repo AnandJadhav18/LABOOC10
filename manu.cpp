9e3c68a9fefdf20d3994e5880f1aa76c19bd8524
AnandJadhav18 <avj20102007@gmail.com>
2026-08-06 19:32:38 +0000

    manupulators

---
+++ manu.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main()
+{
+    int basic=950,Allowance=95,Total=545;
+    cout<<setw(10)<<"basic"<<setw(5)<<basic<<endl
+        <<setw(10)<<"Allowance"<<setw(5)<<Allowance<<endl
+        <<setw(10)<<"Total"<<setw(5)<<Total<<endl;
+    return 0;
+}
