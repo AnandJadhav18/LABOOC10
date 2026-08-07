5e360aaeccd3781eb4be6deac05004b91bd5aed9
AnandJadhav18 <avj20102007@gmail.com>
2026-08-07 06:37:40 +0000

    class

--- class.cpp
+++ class.cpp
@@ -21,16 +21,19 @@ void displayDetails() const
 {
     cout<<"\n-----Student Details-----\n";
     cout<<"Name:"<<name<<endl;
-    cout<<"Roll No.:"<<rollNo<<endl;
+    cout<<"Roll No:"<<rollNo<<endl;
     cout<<"Marks:"<<marks<<endl;
 }
 };
 int main()
 {
-    Student s;
+    Student s,s1,s2;
     s.inputDetails();
     s.displayDetails();
+    s1.inputDetails();
+    s1.displayDetails();
+    s2.inputDetails();
+    s2.displayDetails();
     return 0;
-
 
 }
